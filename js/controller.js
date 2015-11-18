b58058bbb18254d05402e299ec321d6fdf7d33ee
FreddyCalk <freddycalk@gmail.com>
2015-11-18 14:36:14 -0500

    Stock app
    added some styling, and an active class for the stock selected to show
    information in the rest of the app

--- js/controller.js
+++ js/controller.js
@@ -4,17 +4,33 @@ angular.module('stockApp',[]).controller('stockController',function ($scope, $ht
 		var encodedTickers = encodeURIComponent($scope.userStocks);
 		var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20("'+encodedTickers+'")%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
 		
-		$http.get(url).success(function (stockData){
+		// $http.get(url).success(function (stockData){
 
-			if($scope.userStocks.indexOf(',') === -1){
-				listOfStocks = [stockData.query.results.quote];
-			}else{
-				listOfStocks = stockData.query.results.quote;
-			}
-			$scope.listOfStocks = listOfStocks;
+		// 	if($scope.userStocks.indexOf(',') === -1){
+		// 		listOfStocks = [stockData.query.results.quote];
+		// 	}else{
+		// 		listOfStocks = stockData.query.results.quote;
+		// 	}
+			$scope.listOfStocks = [
+			{
+				Symbol: "GOOG",
+				Change: '+2.5%',
+				Name: "Google"
+			},
+			{
+				Symbol: "AAPL",
+				Change: "-1.8%", 
+				Name: "Apple, Inc."
+			},
+			{
+                Symbol: "X",
+                Change: "+2.2",
+                Name: "Trinity Trains"
+            }];
 		
-			// $scope.loadStock = function ($scope)
-		})
+		// 	// $scope.loadStock = function ($scope)
+		// })
+		$('#search-bar').val('');
 	}
 	$scope.loadStock = function (stockData){
 		$scope.dataList = [];
@@ -25,6 +41,9 @@ angular.module('stockApp',[]).controller('stockController',function ($scope, $ht
 				val: stockData[name]
 			})
 		}
+
+		$('.stock-container').removeClass('active');
+		$('#'+this.stock.Symbol).addClass('active');
 	}
 	$scope.getChangeClass = function (change){
 		if(change.indexOf('+') > -1){
