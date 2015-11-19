98092d43a532340c39a2b5698a165e057f82b985
FreddyCalk <freddycalk@gmail.com>
2015-11-18 19:51:41 -0500

    angular stock app
    added some styling, and removed some extraneous information from the
    controller

--- js/controller.js
+++ js/controller.js
@@ -4,32 +4,17 @@ angular.module('stockApp',[]).controller('stockController',function ($scope, $ht
 		var encodedTickers = encodeURIComponent($scope.userStocks);
 		var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20("'+encodedTickers+'")%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
 		
-		// $http.get(url).success(function (stockData){
+		$http.get(url).success(function (stockData){
 
-		// 	if($scope.userStocks.indexOf(',') === -1){
-		// 		listOfStocks = [stockData.query.results.quote];
-		// 	}else{
-		// 		listOfStocks = stockData.query.results.quote;
-		// 	}
-			$scope.listOfStocks = [
-			{
-				Symbol: "GOOG",
-				Change: '+2.5%',
-				Name: "Google"
-			},
-			{
-				Symbol: "AAPL",
-				Change: "-1.8%", 
-				Name: "Apple, Inc."
-			},
-			{
-                Symbol: "X",
-                Change: "+2.2",
-                Name: "Trinity Trains"
-            }];
+			if($scope.userStocks.indexOf(',') < 0){
+				listOfStocks = [stockData.query.results.quote];
+			}else{
+				listOfStocks = stockData.query.results.quote;
+			}
+			$scope.listOfStocks = listOfStocks;
 		
 		// 	// $scope.loadStock = function ($scope)
-		// })
+		})
 		$('#search-bar').val('');
 	}
 	$scope.loadStock = function (stockData){
@@ -41,9 +26,13 @@ angular.module('stockApp',[]).controller('stockController',function ($scope, $ht
 				val: stockData[name]
 			})
 		}
-
+		getChart(stockData);
 		$('.stock-container').removeClass('active');
 		$('#'+this.stock.Symbol).addClass('active');
+	}
+	function getChart(stockData){
+		$scope.image1 = 'http://chart.finance.yahoo.com/z?s='+stockData.Symbol+'&t=3m&q=l&l=on&z=s&p=m10,m100';
+		$scope.image2 = 'http://chart.finance.yahoo.com/z?s='+stockData.Symbol+'&t=1ym&q=l&l=on&z=s&p=m50,m200';
 	}
 	$scope.getChangeClass = function (change){
 		if(change.indexOf('+') > -1){
